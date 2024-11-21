7a22e964836d07aea987c128dd7f03d5f25eb428
João Victor Calvet <joaovictor.calvet@outlook.com>
2024-11-21 13:42:38 -0300

    [Feat] Aula - 04 Commit Inicial

---
+++ Aula - 04/index.js
@@ -0,0 +1,8 @@
+/*
+    Aula - 04: Plataforma de Perguntas e Respostas
+    Primeiramente vamos instalar o EJS, que é uma biblioteca de templates HTML,
+    ou seja, com ele, podemos renderizar HTML no backend e enviar para o Front.
+    Ele tem a funcionalidade de adicionar lógica JavaScript ao HTML.
+
+    Para instalar utilizamos o comando: "npm install ejs --save"
+*/
