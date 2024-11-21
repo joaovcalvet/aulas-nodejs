b6703ffb7a5db6ca311e042db62a3dba953c3d1b
João Victor Calvet <joaovictor.calvet@outlook.com>
2024-11-21 10:04:21 -0300

    [Feat] Aula 01 - Módulos

---
+++ Aula - 01/app.js
@@ -0,0 +1,23 @@
+/*
+    Aula - 01: Módulos
+    Módulos são uma forma de separar código em vários arquivos.
+    Módulos também servem para puxar bibliotecas para o projeto.
+
+    Ao utilizar código de um arquivo separado do que está sendo trabalhado atualmente,
+    é necessário que o trecho de código/função no outro arquivo tenha sido exportada.
+*/
+
+const math = require("./functions/math.js")
+console.log(math.sum(1, 2))
+
+/*
+    Pesquisar:
+    - HTTP
+*/
+
+/*
+    HTTP:
+    Hypertext Transfer Protocol - Protocolo de Transferência de Hypertexto
+*/
+
+// ----------------------------------------------------------------------------------------
