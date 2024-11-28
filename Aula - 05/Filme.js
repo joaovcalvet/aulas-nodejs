b28b2fda49ba5136e330892a23d0decd4ddd3a14
João Victor Calvet <joaovictor.calvet@outlook.com>
2024-11-28 09:35:17 -0300

    [Feat] Conceito OOP

---
+++ Aula - 05/Filme.js
@@ -0,0 +1,27 @@
+class Filme
+{
+    constructor(){
+        this.titulo = "";
+        this.ano = 2000;
+        this.genero = "";
+        this.diretor = "";
+        this.atores = [];
+        this.duracao = 0;
+    }
+
+    Reproduzir(){
+        console.log("Reproduzindo...");
+    }
+
+    Pausar(){
+        console.log("Pausado ||");
+    }
+
+    Avancar(){
+        console.log("Avançar >>");
+    }
+
+    Fechar(){
+        console.log("Fechar X");
+    }
+}
