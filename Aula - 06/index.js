bd6dd50e806d74dc87f3a6dd71bf26913e3afbd0
João Victor Calvet <joaovictor.calvet@outlook.com>
2025-02-17 09:24:07 -0300

    [Feat] Escritor de Arquivos

--- Aula - 06/index.js
+++ Aula - 06/index.js
@@ -6,11 +6,21 @@
     Arquivos Binários: Arquivos codificados em bites, humanamente impossível de ler e interpretar.
 
     Aula - 06.1: Leitura de Arquivos
-    Módulo leitor de arquivos: fs (File System)
-    Esse módulo é padrão do Node.js
+    Módulo para arquivos: fs (File System)
+    Esse módulo é padrão do Node.js.
+    Método "readFile('caminho do arquivo', 'opções', callback)".
+
+    Aula - 06.2: Escrita em Arquivos
+    Utilizando o módulo fs, vamos escrever em um arquivo.
+    Método "writeFile('caminho do arquivo', 'Novo conteúdo a ser escrito', callback)";
 */
 
 import fs from "fs";
+
+fs.writeFileSync("./texto.txt", "Novo texto enviado", function(erro) {
+    if(erro)
+        console.log("O que aconteceu aqui?: ", erro);
+});
 
 fs.readFile("./texto.txt", {encoding: 'utf-8'}, function(erro, dados) {
 
