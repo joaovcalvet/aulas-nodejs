7164c865949e47a673613c0ef3019a0b33d0f5c5
João Victor Calvet <joaovictor.calvet@outlook.com>
2025-02-19 10:02:08 -0300

    [Feat] Consts e Let

--- Aula 07 - ES6 & ES7 & ES8/index.js
+++ Aula 07 - ES6 & ES7 & ES8/index.js
@@ -3,4 +3,62 @@
     Ecma é uma organização que define algumas normas da web.
     O EcmaScript são especificações que a Ecma define para o JS.
     Responsáveis por atualizações na linguagem.
+
+    Aula 07.2: Constantes
+    Constantes são variáveis com valores fixos
+
+    Aula 07.3: Variáveis Let
+    Forma de declarar uma variável onde sua declaração também
+    depende do escopo de bloco.
 */
+
+const nome = "João";
+// -------------------------------------------------
+
+//Diferença de Escopo
+var idade = 21; //Global e local
+let sobrenome = "Victor"; //Global, local e bloco
+// -------------------------------------------------
+
+//Escopo Global
+var nomeVar = "Fabio";
+let nomeLet = "Joana";
+
+function func1() {
+    //Escopo Local
+    var nomeVarLocal = "Marcela";
+    let nomeLetLocal = "Lucas";
+
+    //Acesso a variáveis globais no escopo local
+    console.log(nomeVar + " e " + nomeVarLocal);
+    console.log(nomeLet + " e " + nomeLetLocal);
+}
+
+func1();
+
+//Sem acesso a variáveis locais no escopo global
+try {
+    //Um erro será disparado
+    console.log(nomeVar + " e " + nomeVarLocal);
+    console.log(nomeLet + " e " + nomeLetLocal);
+} catch (error) {
+    console.log(error);
+}
+
+//------------------------------------------------
+
+if(true) {
+    //Escopo de bloco
+    var nomeVarBloco = "Maria";
+    let nomeLetBloco = "Bianca";
+}
+
+//Sem erro ao acessar var de bloco no escopo global
+console.log(nomeVarBloco);
+
+//Erro ao acessar let de bloco no escopo global
+try {
+    console.log(nomeLetBloco);
+} catch (error) {
+    console.log(error);
+}
