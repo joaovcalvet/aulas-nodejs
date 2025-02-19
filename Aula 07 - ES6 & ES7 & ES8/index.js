752612ce880450b30fd95e55f871b9167934a06d
João Victor Calvet <joaovictor.calvet@outlook.com>
2025-02-19 09:41:38 -0300

    [Feat] Arquivos Iniciais

---
+++ Aula 07 - ES6 & ES7 & ES8/index.js
@@ -0,0 +1,6 @@
+/*
+    Aula 07.1: EcmaScript
+    Ecma é uma organização que define algumas normas da web.
+    O EcmaScript são especificações que a Ecma define para o JS.
+    Responsáveis por atualizações na linguagem.
+*/
